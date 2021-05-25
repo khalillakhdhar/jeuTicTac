890a16fb6c8b9c8da757e32f9b1b1add34952ae2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-25 13:08:15 +0100

    preparation

--- src/jeuTicTac/TicTacToeTest.java
+++ src/jeuTicTac/TicTacToeTest.java
@@ -1,5 +1,11 @@
 package jeuTicTac;
 
 public class TicTacToeTest {
-
+		@Rule
+		public ExpectedException exception = ExpectedException.none();
+		private TictTacToe ticTacToe;
+		@Before
+		public final void before() {
+		ticTacToe = new TictTacToe();
+		}
 }
